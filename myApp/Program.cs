5e4af74b8297db4bfbf11a8ed56ea9d4e2bc280e
Muhammad Yasir <yasirbhutta@gmail.com>
2018-02-21 13:41:09 +0500

    commit

---
+++ myApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 100;
+            Console.WriteLine($"Testing ....!{x}");
+        }
+    }
+}

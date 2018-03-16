1b31a32a384d4cb43e573d05f37aef24ae54ce36
Muhammad Yasir Bhutta <yasirbhutta@gmail.com>
2018-03-16 22:28:14 +0500

    dd

--- classes/Program.cs
+++ classes/Program.cs
@@ -6,7 +6,9 @@ namespace classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StructDemo();
+            //
+            Console.ReadKey();
         }
         //
         // struct

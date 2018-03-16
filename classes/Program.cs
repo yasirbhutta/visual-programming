53425a9a69f5425a28add111ca86a98e5048d0ee
Muhammad Yasir Bhutta <yasirbhutta@gmail.com>
2018-03-16 22:25:24 +0500

    ccc

---
+++ classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //
+        // struct
+        //
+        struct Books {
+            public string title;
+            public string author;
+        };
+        static void StructDemo () {
+            Books book1, book2;
+            book1.title = "C# 5.0";
+            book1.author = "Herbert Schildt";
+            book2.title = "Network basics";
+            book2.author = "Muhammad Umer";
+            // print book1 info
+            Console.WriteLine ("***** " + book1.title + " *****");
+            Console.WriteLine (book1.author);
+            // print book2 info
+
+            Console.WriteLine ("***** " + book2.title + " *****");
+            Console.WriteLine (book2.author);
+
+        }
+    }
+}

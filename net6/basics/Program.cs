5ec0bd781b7803cc377d1aacd05765b7da682971
Yasir Bhutta <yasirbhutta@gmail.com>
2022-05-12 07:52:21 +0500

    update collection

--- net6/basics/Program.cs
+++ net6/basics/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpBasics;
+//BCollections obj = new BCollections();
+//BCollections.ArrayDemo();
+//BCollections.ListDemo1();
+// BCollections.ListDemo2();
+// BCollections.ListDemo3();
+// BCollections.ListDemo4();
+BCollections.EnumDemo();
 // DataTypes.FindRadius();
 //DataTypes.Trigonometry();
 //DataTypes.UseDecimal();
@@ -15,4 +22,4 @@ using CSharpBasics;
 //ScopeCasting.NestVar();
 //ScopeCasting.LtoD();
 //ScopeCasting.BytetoInt();
-ScopeCasting.CastDemo();
+//ScopeCasting.CastDemo();

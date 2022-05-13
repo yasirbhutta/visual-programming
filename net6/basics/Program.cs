f692edc91102d38980fe93fb2a0bfb35ed6965f1
Yasir Bhutta <yasirbhutta@gmail.com>
2022-05-13 09:30:41 +0500

    change

--- net6/basics/Program.cs
+++ net6/basics/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpBasics;
+//ControlStatements.PosNeg();
+//ControlStatements.PosNegZero();
+//ControlStatements.SwitchDemo();
+//ControlStatements.SwitchDemo2();
+//ControlStatements.EmptyCasesCanFall();
+//ControlStatements.Comma();
+//ControlStatements.InfFor();
+//ControlStatements.WhileDemo();
+ControlStatements.GotoDemo();
 //BCollections obj = new BCollections();
 //BCollections.ArrayDemo();
 //BCollections.ListDemo1();
@@ -8,7 +17,7 @@ using CSharpBasics;
 // BCollections.ListDemo4();
 //BCollections.EnumDemo();
 //ControlStatements.EmptyCasesCanFall();
-ControlStatements.GotoDemo();
+//ControlStatements.GotoDemo();
 // DataTypes.FindRadius();
 //DataTypes.Trigonometry();
 //DataTypes.UseDecimal();
